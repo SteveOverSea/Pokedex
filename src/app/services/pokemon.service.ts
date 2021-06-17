03fb0816a2cd0bae96eb2899cd5d28fd53ac6aa4
Stefan Rausch <rausch.stefan@edumail.at>
2021-06-17 23:12:30 +0200

    Refactor and collect evolutionchain
    - collect all evolutions recursively
    - adjust details component to show data

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, toArray } from 'rxjs/operators';
+import { map, mapTo, mergeMap, switchMap, take, toArray } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 import { Pokemon } from "../types/Pokemon";
+import { PokeEvolution } from '../types/PokeEvolution';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,8 @@ export class PokemonService {
 
   getAllPokemon(): Observable<string> {
     const url: string = `${this.baseUrl}/pokedex/1`;
-    return this.http.get<any>(url)
+
+    return this.http.get(url)
       .pipe(
           map(( data: any ) => data.pokemon_entries
             .map(( entry: any ) => entry.pokemon_species.name)
@@ -26,27 +28,65 @@ export class PokemonService {
 
   getPokemonDetails(no: number): Observable<Pokemon> {
     const url: string = `${this.baseUrl}/pokemon/${no}`;
-    return this.http.get<any>(url)
+
+    return this.http.get(url)
       .pipe(
+        mergeMap(( pokeData: any ) => {
+          return this.http.get(pokeData.species.url)
+            .pipe(mergeMap(( speciesData: any ) => {
+              return this.http.get(speciesData.evolution_chain.url)
+                .pipe(map(( evolutionData: any ) => {
+                  return {
+                    pokeData,
+                    evolutionData
+                  }
+                }))
+            }))
+        }),
         map(this.refactorPokemonDetails),
       );
   }
 
   refactorPokemonDetails( data: any ): Pokemon {
+    const pokeData = data.pokeData;
+    const evolutionData = data.evolutionData;
     return {
-      picture: data.sprites.front_default,
-      name: data.name,
-      abilities: data.abilities.map(( ability: any ) => ability.ability.name),
-      types: data.types.map(( type: any ) => type.type.name),
-      orderNumber: data.id,
-      stats: data.stats.map(( stat: any) => { 
-        return { name: stat.stat.name, 
-                  value: stat.base_stat
+      picture: pokeData.sprites.front_default,
+      name: pokeData.name,
+      abilities: pokeData.abilities.map(( ability: any ) => ability.ability.name),
+      types: pokeData.types.map(( type: any ) => type.type.name),
+      orderNumber: pokeData.id,
+      stats: pokeData.stats.map(( stat: any) => { 
+        return { 
+          name: stat.stat.name, 
+          value: stat.base_stat
         }
       }),
-      possibleEvolutions: ["..", "..", "fetch from pokemon species"],
-      moves: data.moves.map(( move: any ) => move.move.name)
+      possibleEvolutions: PokemonService.refactorEvolutionChain(evolutionData.chain),
+      moves: pokeData.moves.map(( move: any ) => move.move.name)
     }
+  }
 
+  static refactorEvolutionChain( data: any ): PokeEvolution[] {
+    let evolution: PokeEvolution[] = [];
+
+    
+    PokemonService.collectEvolutions(evolution, data);
+  
+    return evolution;
+  }
+
+  static collectEvolutions(collect: PokeEvolution[], evolution: any) {
+    let pokeEvolution: PokeEvolution = { name: "", id: ""};
+
+    pokeEvolution.name = evolution.species.name;
+    pokeEvolution.id = evolution.species.url.substring("https://pokeapi.co/api/v2/pokemon-species/".length);
+
+    collect.push(pokeEvolution);
+
+    if (evolution.evolves_to.length) {
+      evolution.evolves_to.forEach(( nextEvolution: any ) => 
+        PokemonService.collectEvolutions(collect, nextEvolution))
+    }
   }
 }
