16401ba9be76ec734fdc2be9f750e3af55e5ccac
Stefan Rausch <rausch.stefan@edumail.at>
2021-06-18 00:12:06 +0200

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonListItemComponent } from './pokemon-list-item/pokemon-list-item.component';
 import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonListComponent,
     PokemonListItemComponent,
-    PokemonDetailsComponent
+    PokemonDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
